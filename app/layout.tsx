ed4d16cdee5348ff027400c26e1f1b6c6f8e01f4
Alejandro <smarketing.cuz@gmail.com>
2025-01-02 23:50:06 +0100

    comented code until it's ready for use

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./components/provider";
 import { Analytics } from "@vercel/analytics/react";
-import Image from "next/image";
-import { ConnectWalletButton } from "./components/ConnectButton";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -48,7 +46,8 @@ export default function RootLayout({
       >
         <Providers>{children}</Providers>
         <Analytics />
-        <Providers>
+
+        {/* <Providers>
           <main className="relative max-w-7xl mx-auto">
             <div className="fixed inset-0 w-full h-full bg-repeat bg-noise opacity-20 bg-[length:350px] z-[-20] before:content-[''] before:absolute before:w-[2500px] before:h-[2500px] before:rounded-full before:blur-[100px] before:-left-[1000px] before:-top-[2000px] before:bg-sky-500 before:opacity-50 before:z-[-100] after:content-[''] after:absolute after:w-[2500px] after:h-[2500px] after:rounded-full after:blur-[100px] after:-right-[1000px] after:-bottom-[2000px] after:bg-sky-500 after:opacity-50 after:z-[-100]"></div>
             <nav className="flex relative items-center justify-between py-6">
@@ -57,7 +56,7 @@ export default function RootLayout({
             </nav>
             {children}
           </main>
-        </Providers>
+        </Providers> */}
       </body>
     </html>
   );
