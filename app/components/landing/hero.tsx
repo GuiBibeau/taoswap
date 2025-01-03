47488b0418ec252bc1ebe37f10f8c6ba284adecf
guibibeau <g.bibeaulaviolette@gmail.com>
2025-01-03 13:25:57 -0500

    feat: adding github link

--- app/components/landing/hero.tsx
+++ app/components/landing/hero.tsx
@@ -1,9 +1,7 @@
 "use client";
-import { IconArrowRight } from "@tabler/icons-react";
 import Link from "next/link";
 import React from "react";
 import Form from "next/form";
-
 import { TopGradient } from "./top-gradient";
 import { BottomGradient } from "./bottom-gradient";
 import { TopLines } from "./top-lines";
@@ -11,6 +9,7 @@ import { BottomLines } from "./bottom-lines";
 import { SideLines } from "./side-lines";
 import { subscribeToWaitlist } from "@/app/actions/subscribe-to-waitlist";
 import { SubmitButton } from "./submit-button";
+import { Star, ArrowRight } from "lucide-react";
 
 export function Hero() {
   return (
@@ -25,15 +24,13 @@ export function Hero() {
 
           <div className="relative z-20 flex flex-col items-center justify-center overflow-hidden rounded-3xl p-4 md:p-12">
             <Link
-              href="#"
-              className="flex items-center gap-1 rounded-full border border-[#404040] bg-gradient-to-b from-[#5B5B5D] to-[#262627] px-4 py-1 text-center text-sm text-white"
+              href="https://github.com/GuiBibeau/taoswap"
+              className="flex items-center gap-1.5 rounded-full border border-[#404040] bg-gradient-to-b from-[#5B5B5D] to-[#262627] px-4 py-1 text-center text-sm text-white"
             >
-              <span>First DEX on Bittensor</span>
-              <IconArrowRight className="h-4 w-4 text-white" />
+              <Star className="w-4 h-4" />
+              <span>Star on Github!</span>
+              <ArrowRight className="w-4 h-4" />
             </Link>
-            <h1 className="bg-gradient-to-b from-black to-neutral-600 bg-clip-text py-4 text-center text-2xl text-transparent dark:from-white dark:to-[#999] md:text-4xl lg:text-7xl">
-              τaoSwap: <br /> Bittensor&apos;s DeFi Hub
-            </h1>
             <p className="mx-auto max-w-2xl py-4 text-center text-base text-neutral-600 dark:text-neutral-300 md:text-lg">
               The first DEX built on Bittensor, TaoSwap enables secure TAO
               trading and DeFi opportunities. Built for the community, we
