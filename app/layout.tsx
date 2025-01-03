4c0c5dc17940d5edf077959811404c5798a61a49
guibibeau <g.bibeaulaviolette@gmail.com>
2025-01-03 13:14:31 -0500

    feat: fix landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./components/provider";
 import { Analytics } from "@vercel/analytics/react";
-import { ConnectWalletButton } from "./components/connect-button";
-import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -57,16 +55,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-zinc-900 dark:text-zinc-50`}
       >
         <Analytics />
-
         <Providers>
-          <main className="relative max-w-7xl mx-auto">
-            <div className="fixed inset-0 w-full h-full bg-repeat bg-noise opacity-20 bg-[length:350px] z-[-20] before:content-[''] before:absolute before:w-[2500px] before:h-[2500px] before:rounded-full before:blur-[100px] before:-left-[1000px] before:-top-[2000px] before:bg-sky-500 before:opacity-50 before:z-[-100] after:content-[''] after:absolute after:w-[2500px] after:h-[2500px] after:rounded-full after:blur-[100px] after:-right-[1000px] after:-bottom-[2000px] after:bg-sky-500 after:opacity-50 after:z-[-100]"></div>
-            <nav className="flex relative items-center justify-between py-6">
-              <Image src="/logo.svg" alt="logo" width={150} height={100} />
-              <ConnectWalletButton />
-            </nav>
-            {children}
-          </main>
+          <main className="relative max-w-7xl mx-auto">{children}</main>
         </Providers>
       </body>
     </html>
---
+++ app/swap/layout.tsx
@@ -0,0 +1,19 @@
+import { ConnectWalletButton } from "../components/connect-button";
+import Image from "next/image";
+
+export default function SwapLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <div className="fixed inset-0 w-full h-full bg-repeat bg-noise opacity-20 bg-[length:350px] z-[-20] before:content-[''] before:absolute before:w-[2500px] before:h-[2500px] before:rounded-full before:blur-[100px] before:-left-[1000px] before:-top-[2000px] before:bg-sky-500 before:opacity-50 before:z-[-100] after:content-[''] after:absolute after:w-[2500px] after:h-[2500px] after:rounded-full after:blur-[100px] after:-right-[1000px] after:-bottom-[2000px] after:bg-sky-500 after:opacity-50 after:z-[-100]"></div>
+      <nav className="flex relative items-center justify-between py-6">
+        <Image src="/logo.svg" alt="logo" width={150} height={100} />
+        <ConnectWalletButton />
+      </nav>
+      {children}
+    </>
+  );
+}
