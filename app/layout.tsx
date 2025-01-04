256e709b96a8edb0ebe5c9a6ba22845fb7a24bb5
guibibeau <g.bibeaulaviolette@gmail.com>
2025-01-04 16:58:19 -0500

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import { ContextProvider } from "./components/provider";
 import { ThemeProvider } from "next-themes";
-
-import { headers } from "next/headers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
